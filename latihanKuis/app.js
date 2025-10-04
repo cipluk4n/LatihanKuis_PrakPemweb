6b79b649e6fbaea4ec231de03ff954ee39fb2e13
cipluk4n <asiilrabbani@gmail.com>
2025-10-04 16:57:46 +0700

    Add files via upload

---
+++ latihanKuis/app.js
@@ -0,0 +1,14 @@
+new Swiper('.card-wrapper', {
+  loop: true,
+
+  // Pagination bulltets
+  pagination: {
+    el: '.arrow',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.left',
+    prevEl: '.right',
+  },
+});
